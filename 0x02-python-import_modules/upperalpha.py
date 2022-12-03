8cdd4bf057eb9fc604610bd6d9d642ca04a49ca7
Martins Ndifon <martinsndifon@gmail.com>
2022-12-03 19:44:31 +0000

    update to upperalpha

--- 0x02-python-import_modules/upperalpha.py
+++ 0x02-python-import_modules/upperalpha.py
@@ -4,3 +4,9 @@ for i in range(65, 91):
         print("{}".format(chr(i)), end='')
     else:
         print("{}".format(chr(i)))
+
+'''
+you can also write it this way with import
+from string import ascii_uppercase
+print(ascii_uppercase)
+
