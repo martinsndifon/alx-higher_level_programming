3e030c3639a23b7a7cb139a1b05cfc5e8cf625b7
Martins Ndifon <martinsndifon@gmail.com>
2022-12-03 13:51:17 +0000

    update to 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    i = 1
+    lenght = len(sys.argv) - 1
+
+    if lenght == 0:
+        print("{:d} arguments.".format(lenght))
+    elif lenght == 1:
+        print("{:d} argument:".format(lenght))
+        print("{}: {}".format(lenght, sys.argv[1]))
+    else:
+        print("{:d} argument:".format(lenght))
+        while i <= lenght:
+            print("{}: {}".format(i, sys.argv[i]))
+            i += 1
+
+
+if __name__ == "__main__":
+    main()
