c673806f6070c6d3259e2fb8dcfe1e542deeec2e
Martins Ndifon <martinsndifon@gmail.com>
2022-12-03 14:35:03 +0000

    update to 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import variable_load_5
+from variable_load_5 import a
 
 
 def main():
-    print("{}".format(variable_load_5.a))
+    print("{}".format(a))
 
 
 if __name__ == "__main__":
