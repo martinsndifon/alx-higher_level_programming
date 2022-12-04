3f511e974be1db669bf350657143cb665a7c1873
Martins Ndifon <martinsndifon@gmail.com>
2022-12-04 19:28:46 +0000

    update to 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    new_string = ''
+
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            continue
+        else:
+            new_string += c
+
+    return new_string
