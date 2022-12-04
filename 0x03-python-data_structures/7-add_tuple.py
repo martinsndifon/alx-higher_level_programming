e45bdaa89522fa3dfecf9860de3e260bb82935aa
Martins Ndifon <martinsndifon@gmail.com>
2022-12-04 20:40:53 +0000

    update to 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = 0, 0
     if not tuple_b:
         tuple_b = 0, 0
-    
+
     if len(tuple_a) == 1:
         tuple_a = tuple_a[0], 0
     if len(tuple_b) == 1:
