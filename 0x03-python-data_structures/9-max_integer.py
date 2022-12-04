58dcbec89223af2acd905f1ca256a34547686834
Martins Ndifon <martinsndifon@gmail.com>
2022-12-04 20:53:14 +0000

    update to 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    maxnum = my_list[0]
+
+    for num in my_list:
+        if num > maxnum:
+             maxnum = num
+    return maxnum
