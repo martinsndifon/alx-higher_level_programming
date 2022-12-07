3a949ca1459e27c5e56bedd4f44ad44978f48ed1
Martins Ndifon <martinsndifon@gmail.com>
2022-12-07 22:30:05 +0000

    update to 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    i = 0
+
+    for item in a_dictionary.keys():
+        i += 1
+
+    return i
+'''
+You can also use len(a_dictionary). 
+I just like custom functions for som weird reason
+'''
