4df6a8d6be910c1747bd2ae938521e619ec4d9cc
Martins Ndifon <martinsndifon@gmail.com>
2022-12-09 16:05:36 +0000

    update to 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -18,15 +18,15 @@ def roman_to_int(roman_string):
       # roman_string = roman_string.replace("XL", "XXXX").replace("XC", "LXXXX")
       # roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
 
-      num = 0
-      _len = len(roman_string)
+    num = 0
+    _len = len(roman_string)
 
-      for i in range(_len):
-          if not rom_to_int.get(roman_string[i]):
-              return 0
-          value = rom_to_int[roman_string[i]]
-          if i != _len - 1 and rom_to_int[roman_string[i + 1]] > value:
-              num -= value
-          else:
-              num += value
-        return num
+    for i in range(_len):
+        if not rom_to_int.get(roman_string[i]):
+            return 0
+        value = rom_to_int[roman_string[i]]
+        if i != _len - 1 and rom_to_int[roman_string[i + 1]] > value:
+            num -= value
+        else:
+            num += value
+    return num
