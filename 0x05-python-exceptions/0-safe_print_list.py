289ea60195b79aa10057309ee0fce1e37cb611a5
Martins Ndifon <martinsndifon@gmail.com>
2022-12-19 11:58:03 +0000

    update to 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        for item in my_list:
+            i += 1
+
+        if x > i:
+            x = i
+
+        j = 0
+        while j < x:
+            print(my_list[j], end='')
+            j += 1
+        print()
+
+        return j
+    except:
+        pass
