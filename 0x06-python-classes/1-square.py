a8819c8d103ca91a96489c7f309916f9d8b4ba9a
Martins Ndifon <martinsndifon@gmail.com>
2022-12-21 20:22:14 +0000

    update to 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""class definition"""
+
+
+class Square:
+    """A class definition of a square"""
+    def __init__(self, size):
+        """Initialize the square size"""
+
+        self.__size = size
