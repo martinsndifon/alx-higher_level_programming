475ed9966c513d8b5c467af716ac4965ecdba6fd
Martins Ndifon <martinsndifon@gmail.com>
2022-12-29 08:36:16 +0000

    update to 4-print_square.py module

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""
+A module that prints a square using the '#' symbol
+"""
+
+def print_square(size):
+    """
+    prints a square uning the '#' symbol with size, size
+    - size must be an integer
+    - size must be greater than or less than 0
+    - size cannot be of type float
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print(f'#' * size)
