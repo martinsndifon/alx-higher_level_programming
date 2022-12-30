ef6383e64b7c090f2abf99f1161ed996d57de56d
Martins Ndifon <martinsndifon@gmail.com>
2022-12-30 11:03:44 +0000

    update to 5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""
+A module that prints two new lines after the characters . ? and :
+"""
+
+
+def text_indentation(text):
+
+    """
+    prints two new lines after the characters . ? and :
+    - text must be a string
+    - No spaces at the beginning or at the end of each printed line
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(f'{text[i]}', end='')
+            print('\n')
+            i += 1
+            if text[i] == ' ':
+                i += 1
+            else:
+                continue
+        else:
+            print(f'{text[i]}', end='')
+            i += 1
