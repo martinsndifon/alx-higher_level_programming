9c188b4ec0525eaad03eb64a28b6c21db8f95098
Martins Ndifon <martinsndifon@gmail.com>
2023-01-02 17:44:23 +0000

    update to 9-rectangle module

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -136,5 +136,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Returns width == height == size"""
-        
-        return  cls(size, size)
+
+        return cls(size, size)
