e39b81b0f808689fd76073f0059176961b5a379b
Martins Ndifon <martinsndifon@gmail.com>
2023-01-02 13:10:49 +0000

    corrected pycodestyle errors

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -37,12 +37,11 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
-
 
     @property
     def height(self):
@@ -56,7 +55,7 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        
+
         if value < 0:
             raise ValueError("height must be >= 0")
 
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,12 +37,11 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
-
 
     @property
     def height(self):
@@ -56,7 +55,7 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        
+
         if value < 0:
             raise ValueError("height must be >= 0")
 
@@ -70,7 +69,7 @@ class Rectangle:
 
     def perimeter(self):
         """computes the perimeter of the rectangle"""
-        
+
         if not self.__height or not self.__width:
             return 0
 
