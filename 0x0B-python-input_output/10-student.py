097a7c9972a992e45631ca07b6178dd8c615321d
Martins Ndifon <martinsndifon@gmail.com>
2023-01-10 18:27:40 +0000

    update to 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""A student class"""
+
+
+class Student:
+    """A class Student that defines a student with set attributes"""
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """Initialize Student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """converts the class to json"""
+    
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
