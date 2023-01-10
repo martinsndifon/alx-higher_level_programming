b65bd4752c3624ae2121b7f978a96b4fb884227a
Martins Ndifon <martinsndifon@gmail.com>
2023-01-10 16:13:14 +0000

    update to 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""A module to return an object represented by a JSON string"""
+import json
+
+def from_json_string(my_str):
+    """Return the object represented by a JSON string"""
+
+    return json.loads(my_str)
