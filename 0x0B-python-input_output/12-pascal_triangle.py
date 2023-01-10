2c0f75359646c033946e9dbb0c30004cd0a32533
Martins Ndifon <martinsndifon@gmail.com>
2023-01-10 21:00:49 +0000

    update to the pascal triangle problem

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Pascal Triangle Module"""
+
+
+def pascal_triangle(n):
+    """computes the pascal triangle for n rows"""
+
+    if n <= 0:
+        return []
+
+    res = [[1]]
+
+    for i in range (n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+
+    return res
+
