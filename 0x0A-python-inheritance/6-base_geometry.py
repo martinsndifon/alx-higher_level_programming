8bbadb5d4171f974404aabcd50e089c919bbfb7d
Martins Ndifon <martinsndifon@gmail.com>
2023-01-11 19:39:58 +0000

    Initial commit for the 6-base_geometry.py module

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""base_geometry module"""
+
+
+class BaseGeometry:
+    """defines the new empty class"""
+    
+    def area(self):
+        """computes the area of a geometric object"""
+        raise Exception("area() is not implemented")
