d9a3ea7745a1c0c21444ece913ca2e43ecb56138
Martins Ndifon <martinsndifon@gmail.com>
2023-03-13 13:00:16 +0000

    update to task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const len = process.argv.length;
 
-if (len === 2) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+	console.log('No argument');
 }
