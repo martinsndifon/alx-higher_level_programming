c7e8076cf09a04feaea29e92b30bfa910ef9e3be
Martins Ndifon <martinsndifon@gmail.com>
2023-03-13 09:46:42 +0000

    Initial commit for task 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const process = require('node:process');
+
+let args = process.argv;
+
+if (args.length === 2) {
+	console.log('No argument');
+} else if (args.length === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
