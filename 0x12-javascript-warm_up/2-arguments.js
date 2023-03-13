5d1ca31b9a0f8f6d7121a48df9aa5db4a08b59b9
Martins Ndifon <martinsndifon@gmail.com>
2023-03-13 11:48:22 +0000

    update to task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,13 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const process = require('node:process');
 
-let args = process.argv;
+const args = process.argv;
 
 if (args.length === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (args.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
