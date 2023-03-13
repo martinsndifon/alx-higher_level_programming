76ce88455460bf747cbeb42df6098b733d3dfd80
Martins Ndifon <martinsndifon@gmail.com>
2023-03-13 15:27:31 +0000

    update to task 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,17 +3,12 @@
 const args = process.argv;
 const num = +args[2];
 
-if (!args[2]) {
-  console.log(1);
-  return;
-}
-
 function factorial (n) {
   if (n < 0) {
     return (-1);
   }
 
-  if (n === 0) {
+  if (n === 0 || !args[2]) {
     return (1);
   }
 
