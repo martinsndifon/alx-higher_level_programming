a4fa085e43057b4f2162fd468e7305bd2b5f20ff
Martins Ndifon <martinsndifon@gmail.com>
2023-03-14 22:31:50 +0000

    update to second advance task

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDict = (dict) => {
+  const newDict = {};
+  for (const k in dict) {
+    if (newDict[dict[k]] === undefined) {
+      newDict[dict[k]] = [];
+    }
+    newDict[dict[k]].push(k);
+  }
+
+  return newDict;
+};
+
+console.log(newDict(dict));
