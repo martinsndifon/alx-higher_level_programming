cddde94786ad64e66f925552f3eef862d4011eb2
Martins Ndifon <martinsndifon@gmail.com>
2023-03-18 15:03:42 +0000

    Initial solution for tasks 1,2,3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Lists all states with name starting with 'N' from the database
+hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+def main():
+    """The query below could also be written as 
+    ...name LIKE BINARY UPPER('N%')..."""
+
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states 
+                 WHERE name REGEXP '^N.*$' ORDER BY states.id""")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__ == '__main__':
+    main()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Lists all states with name matching the argument from 
+the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+def main():
+
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states 
+                 WHERE name='{}' ORDER BY states.id""".format(argv[4]))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__ == '__main__':
+    main()
