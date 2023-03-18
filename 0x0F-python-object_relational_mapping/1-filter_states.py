4ac2991f9cec988c4e7154c4e03b08f2c5b6752b
Martins Ndifon <martinsndifon@gmail.com>
2023-03-18 15:41:39 +0000

    Fixed binary error is task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ def main():
         host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                 WHERE BINARY name LIKE REGEXP '^N.*$'
+                 WHERE BINARY name LIKE UPPER('N%')
                  ORDER BY states.id""")
 
     rows = cur.fetchall()
