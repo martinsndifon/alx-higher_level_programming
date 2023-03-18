8e6c5d554a8478d9af728734a2a3336948bc5650
Martins Ndifon <martinsndifon@gmail.com>
2023-03-18 15:36:25 +0000

    Used 'BINARY name' instead of just 'name'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,8 @@ def main():
         host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                 WHERE name REGEXP '^N.*$' ORDER BY states.id""")
+                 WHERE BINARY name LIKE REGEXP '^N.*$'
+                 ORDER BY states.id""")
 
     rows = cur.fetchall()
     for row in rows:
