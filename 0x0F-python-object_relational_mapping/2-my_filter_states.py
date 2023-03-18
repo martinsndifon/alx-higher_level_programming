da10cf5e447bfde386aa3cfc315bbe81db9a4699
Martins Ndifon <martinsndifon@gmail.com>
2023-03-18 15:33:38 +0000

    Used 'LIKE' rather '=' for the sql query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,8 @@ def main():
         host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                 WHERE name LIKE '{}' ORDER BY states.id""".format(argv[4]))
+                 WHERE BINARY name LIKE '{}'
+                 ORDER BY states.id""".format(argv[4]))
 
     rows = cur.fetchall()
     for row in rows:
