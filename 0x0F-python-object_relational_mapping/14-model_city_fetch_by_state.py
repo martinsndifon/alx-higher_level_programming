5cabd2e91c2c538f11f65ce37cd3660b15e81b94
Martins Ndifon <martinsndifon@gmail.com>
2023-03-19 08:34:39 +0000

    Solution to task 14-model_city_fetch_by_state.

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from sys import argv
+from model_state import State, Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import Session
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+    result = session.query(State.name, City.id, City.name).join(
+                           City, State.id == City.state_id).order_by(
+                           City.id).all()
+
+    for state_name, city_id, city_name in result:
+        print(f'{state_name}: ({city_id}) {city_name}')
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+City model - will be the base model for all cities
+"""
+
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import State, Base
+
+
+class City(Base):
+    """Maps this table to the corresponding sql table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
