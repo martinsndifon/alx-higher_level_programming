4c52af8e75fc5e9ed6b344f033e8087c329dc5a5
Martins Ndifon <martinsndifon@gmail.com>
2023-03-30 16:06:49 +0000

    update to task 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# takes in a URL, sends a request to that URL, and displays the size
-# of the body of the response
-
+#!/bin/bash
+# Sends a request to a URL, and displays the size of the body of the response
 curl -sI "$1" | grep -iF "content-length" | cut -d " " -f 2
