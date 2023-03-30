17af680e4756b07cf7c053ed341d7f6fb8a3308a
Martins Ndifon <martinsndifon@gmail.com>
2023-03-30 16:07:15 +0000

    update to task 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# takes in a URL, sends a GET request to that URL, and displays the
-# body of the response only if response code is 200
-
+#!/bin/bash
+# Sends a GET request to a URL, displays the size of the body of the response
 curl -sL "$1"
