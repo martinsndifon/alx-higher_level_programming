236f73224bddb860a5d97298967b97e0723aedc7
Martins Ndifon <martinsndifon@gmail.com>
2023-03-30 16:07:38 +0000

    update to task 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# takes in a URL, sends a DELETE request to that URL, and displays the
-# body of the response
-
+#!/bin/bash
+# Sends a DELETE request to that URL, and displays the body of the response
 curl -sX DELETE "$1"
