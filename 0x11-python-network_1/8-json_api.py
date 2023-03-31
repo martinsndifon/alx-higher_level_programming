9d590e5423f2919de37cb5a4490615b45f05a03c
Martins Ndifon <martinsndifon@gmail.com>
2023-03-31 19:55:00 +0000

    update to task 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST request to passed URL with email as parameter and displays
+the body of the response
+"""
+
+import requests
+import sys
+
+
+def main():
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) < 2:
+        q = {'q': ""}
+    else:
+        q = {'q': sys.argv[1]}
+    res = requests.post(url, data=q)
+    try:
+        obj = res.json()
+        if not obj:
+            print('No result')
+        else:
+            print(f"[{obj.get('id')}] {obj.get('name')}")
+    except Exception:
+        print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    main()
