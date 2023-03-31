1ca5c941dff8660fd6101376d11cc2cf60ea012f
Martins Ndifon <martinsndifon@gmail.com>
2023-03-31 16:10:39 +0000

    update to task 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST request to passed URL with email as parameter and displays
+the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(email)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    byte = response.read()
+print(byte.decode())
