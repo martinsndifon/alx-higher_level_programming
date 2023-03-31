06b087f2d0389f277338b61a9a713c029c5a988a
Martins Ndifon <martinsndifon@gmail.com>
2023-03-31 22:46:11 +0000

    update to the advance task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List 10 commits of a repository by a user using github API"""
+import sys
+import requests
+
+
+def main():
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    query = {'per_page': 10}
+    url = f'https://api.github.com/repos/{user}/{repo}/commits'
+    res = requests.get(url, data=query)
+    objs = res.json()
+
+    for obj in objs:
+        commit = obj['commit']
+        author = commit['author']
+        print(f"{obj.get('sha')}:", author.get('name'))
+
+
+if __name__ == '__main__':
+    main()
