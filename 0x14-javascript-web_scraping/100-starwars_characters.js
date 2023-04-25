33ccd127241333cf90c2eecee65dacf9624bfbb7
Martins Ndifon <martinsndifon@gmail.com>
2023-04-25 17:25:30 +0000

    update to advance task 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+
+const request = require('request');
+const argv = require('process').argv;
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    throw err;
+  }
+  const data = JSON.parse(body).characters;
+  for (const userUrl of data) {
+    request(userUrl, (error, response, value) => {
+      if (error) {
+        throw err;
+      }
+      console.log(JSON.parse(value).name);
+    });
+  }
+});
