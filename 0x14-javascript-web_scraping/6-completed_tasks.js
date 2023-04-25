cfca038e6cbbed8061707935adde0990eb6ccabe
Martins Ndifon <martinsndifon@gmail.com>
2023-04-25 16:33:26 +0000

    update to task 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+//computes number of tasks completed by user id
+
+const request = require('request');
+const argv = require('process').argv;
+
+request(argv[2], (err, res, body) => {
+  if (err) {
+    throw err;
+  }
+  let data = JSON.parse(body);
+  const objs = {};
+  data = data.filter(({ completed }) => completed === true);
+  for (const UserObj of data) {
+    const userId = UserObj.userId;
+    if (objs[userId]) {
+      objs[userId] += 1;
+    } else {
+      objs[userId] = 1;
+    }
+  }
+  console.log(objs);
+});
