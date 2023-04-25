4bb7417c1498f238e86d91fa14d601c464754e0a
Martins Ndifon <martinsndifon@gmail.com>
2023-04-25 13:54:57 +0000

    update to task 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,10 @@
 const fs = require('fs');
 const process = require('process');
 
-const path = process.argv[2]
+const path = process.argv[2];
 fs.readFile(path, 'utf8', (err, data) => {
   if (err) console.log(err);
-  else
+  else {
     console.log(data);
+  }
 });
