b90a8c444c5ed0dfa3ef777ea8e5dfcbaf689d72
martinsndifon <martinsndifon@gmail.com>
2023-05-03 19:55:29 +0100

    new update for task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -4,6 +4,6 @@ $.ajax({
 	type: 'GET',
 	url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
 	success: (data) => {
-		character.text(data.name)
+		character.text(data.name);
 	}
-})
+});
