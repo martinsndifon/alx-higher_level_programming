3617a2e03e0c7b87503fb4be523dfef083c621ae
martinsndifon <martinsndifon@gmail.com>
2023-05-04 00:25:14 +0100

    update to task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -2,8 +2,6 @@ $(function () {
 	const hello = $('#hello');
 	$.ajax({
 		type: 'GET',
-		dataType: "jsonp",
-		jsonpCallback: "callback",
 		url: 'https://fourtonfish.com/hellosalut/?lang=fr',
 		success: (data) => {
 			hello.text(data.hello);	
