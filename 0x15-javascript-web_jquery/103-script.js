a262606944b375407136183cc210ae3e221400ce
martinsndifon <martinsndifon@gmail.com>
2023-05-04 09:51:01 +0100

    update to task 103

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -4,28 +4,24 @@ $(function() {
 	const hello = $('div#hello');
 
 	translate.on('click', () => {
+		const code = langCode.val();
 		$.ajax({
 			type: 'GET',
-			url: 'https://www.fourtonfish.com/hellosalut/hello/',
-			data: {
-				lang: `${langCode}`
-			},
+			url: 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + code,
 			success: (data) => {
-				hello.text(data)
+				hello.text(data.hello);
 			}
 		});
 	});
 
 	langCode.on('keypress', (event) => {
 		if (event.keyCode === 13) {
+			const code = langCode.val();
 			$.ajax({
 				type: 'GET',
-				url: 'https://www.fourtonfish.com/hellosalut/hello/',
-				data: {
-					lang: `${langCode}`
-				},
+				url: 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + code,
 				success: (data) => {
-					hello.text(data);
+					hello.text(data.hello);
 				}
 			});
 		}
